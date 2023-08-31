e8528a141b1d6932074daedf3ff8dd18b282b22a
이태훈 <abc2767@snu.ac.kr>
2023-08-31 18:16:58 +0900

    mvc, oop practice

---
+++ oop-practice/src/test/java/org/example/PasswordValidadtorTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class PasswordValidadtorTest {
+
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
+    @Test
+    void validatePasswordTest() {
+        assertThatCode(() -> PasswordValidator.validate("xognsxogns"))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IllegalArgumentException 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaabbbc", "aaabbbcccdddd"})
+    void validatePasswordTest2(String password){
+        assertThatCode(() -> PasswordValidator.validate(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
+    }
+}
