ad8d773e7af2f1ba6e569734b95373b2d45859a2
이태훈 <abc2767@snu.ac.kr>
2023-08-31 21:10:29 +0900

    calculator practice

---
+++ calculator/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class CalculatorTest {
+
+    // 1 + 2 -----> Calculator
+    //   3   <-----
+    @DisplayName("덧셈 연상을 수행한다.")
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void calculateTest(int operand1, String operator, int operand2, int result) {
+
+        int calculateResult = Calculator.calculate(operand1, operator, operand2);
+
+        assertThat(calculateResult).isEqualTo(result);
+    }
+
+    private static Stream<Arguments> formulaAndResult(){
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
+    }
+}
